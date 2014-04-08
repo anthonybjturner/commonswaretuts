6a46b2a4c12339d3c21407bfbb50cc3316059f30
anthony <anthony@192.168.2.5>
2014-04-08 00:13:51 -0400

    Tutorial seven.

---
+++ EmPubLite/src/com/commonsware/empublite/EmPubLiteActivity.java
@@ -0,0 +1,39 @@
+package com.commonsware.empublite;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+
+public class EmPubLiteActivity extends SherlockFragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.options, menu);
+		return (super.onCreateOptionsMenu(menu));
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		
+			case android.R.id.home:
+				return (true);
+			case R.id.about:
+				return (true);
+			case R.id.help:
+				return (true);
+		}
+		return (super.onOptionsItemSelected(item));
+	}
+
+}
