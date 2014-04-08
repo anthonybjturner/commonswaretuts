65a2f1d4b01c4ad58788c0321cbacf2f6b4a21fd
anthonybjturner <Anthony@192.168.2.13>
2014-04-07 21:25:11 -0400

    Commonsware

---
+++ MainActivity/src/com/commonsware/empublite/EmPubLite.java
@@ -0,0 +1,12 @@
+package com.commonsware.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmPubLite extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
